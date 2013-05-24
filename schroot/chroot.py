75e9f3d3f15865e529f8db2bf0a90e2eb8e11fef
Paul Tagliamonte <tag@pault.ag>
2013-05-23 22:00:04 -0400

    add return checking

--- schroot/chroot.py
+++ schroot/chroot.py
@@ -23,7 +23,7 @@ class SchrootChroot(object):
         self.active = False
 
     def _safe_run(self, cmd):
-        log.debug("Command: %s" % (" ".join(cmd)))
+        # log.debug("Command: %s" % (" ".join(cmd)))
         out, err, ret = run_command(cmd)
         if ret != 0:
             raise SchrootCommandError()
@@ -35,7 +35,8 @@ class SchrootChroot(object):
         return obj['mount-location']
 
     def copy(self, what, whence, user=None):
-        o, e, r = self.run(["mktemp", "-d"])  # Don't pass user.
+        o, e, r = self.run(["mktemp", "-d"],
+                           return_codes=0)  # Don't pass user.
         # it'll set the perms wonky.
         where = o.strip()
         what = os.path.abspath(what)
@@ -45,8 +46,8 @@ class SchrootChroot(object):
         l = l if not l.endswith("/") else l[:-1]
         pth = "%s/%s" % (l, internal)
         shutil.copy(what, pth)
-        self.run(['mv', internal, whence], user=user)
-        self.run(['rm', '-rf', where], user=user)
+        self.run(['mv', internal, whence], user=user, return_codes=0)
+        self.run(['rm', '-rf', where], user=user, return_codes=0)
 
     def get_session_config(self):
         cfg = configparser.ConfigParser()
@@ -64,9 +65,13 @@ class SchrootChroot(object):
     def end(self):
         out, err, ret = self._safe_run(['schroot', '-e', '-c', self.session])
 
-    def run(self, cmd, user=None):
+    def run(self, cmd, user=None, return_codes=None):
         if not isinstance(cmd, list):
             cmd = [cmd]
+
+        if return_codes is not None:
+            if not isinstance(return_codes, tuple):
+                return_codes = (return_codes, )
 
         command = ['schroot', '-r', '-c', self.session]
         if user:
@@ -74,10 +79,14 @@ class SchrootChroot(object):
         command += ['--'] + cmd
         log.debug(" ".join((str(x) for x in command)))
         out, err, ret = run_command(command)
+
+        if ret not in return_codes:
+            raise SchrootCommandError("Bad return code")
+
         return out, err, ret
 
     def __lt__(self, other):
-        return self.run(other)
+        return self.run(other, return_codes=0)
 
     def __floordiv__(self, other):
         return UserProxy(other, self)
@@ -92,8 +101,9 @@ class UserProxy(SchrootChroot):
         for entry in other.__slots__:
             setattr(self, entry, getattr(other, entry))
 
-    def run(self, cmd):
-        return super(UserProxy, self).run(cmd, user=self.user)
+    def run(self, cmd, return_codes=None):
+        return super(UserProxy, self).run(cmd, user=self.user,
+                                          return_codes=return_codes)
 
 
 @contextmanager
